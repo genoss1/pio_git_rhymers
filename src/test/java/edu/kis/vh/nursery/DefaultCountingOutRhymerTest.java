ecc1d2aaa23f5a32947a2d81a26b4f5624bea61f
Mateusz <230056@edu.p.lodz.pl>
2022-05-12 13:21:46 +0200

    14. Testy jednostkowe

---
+++ src/test/java/edu/kis/vh/nursery/DefaultCountingOutRhymerTest.java
@@ -0,0 +1,30 @@
+package edu.kis.vh.nursery;
+
+import junit.framework.TestCase;
+
+public class DefaultCountingOutRhymerTest extends TestCase {
+
+    public void testGetNumbers() {
+    }
+
+    public void testGetTotal() {
+    }
+
+    public void testSetTotal() {
+    }
+
+    public void testCountIn() {
+    }
+
+    public void testCallCheck() {
+    }
+
+    public void testIsFull() {
+    }
+
+    public void testPeekaboo() {
+    }
+
+    public void testCountOut() {
+    }
+}
