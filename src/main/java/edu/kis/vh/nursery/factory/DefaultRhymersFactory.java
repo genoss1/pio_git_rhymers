e4ab00e4b459b36f0fee0872eac1d03115ad845b
Mateusz <230056@edu.p.lodz.pl>
2022-05-12 13:28:36 +0200

    16. Wygenerowanie i uzupełnienie szkieletu dokumentacji do wybranej klasy i jej metod

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -4,23 +4,38 @@ import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FifoRhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
+/**
+ * DefaultRhymersFactory class implements methods from Rhymersfactory interface
+ */
 public class DefaultRhymersFactory implements RhymersFactory {
-
+	/**
+	 * getStandardRhymer()
+	 * @return new DefaultCountingOutRhymer object
+	 */
 	@Override
 	public DefaultCountingOutRhymer getStandardRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
-
+	/**
+	 * getFalseRhymer()
+	 * @return new DefaultCountingOutRhymer object
+	 */
 	@Override
 	public DefaultCountingOutRhymer getFalseRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
-
+	/**
+	 * getFIFORhymer()
+	 * @return new FIFORhymer object
+	 */
 	@Override
 	public DefaultCountingOutRhymer getFifoRhymer() {
 		return new FifoRhymer();
 	}
-
+	/**
+	 * getHanoiRhymer()
+	 * @return new HanoiRhymer object
+	 */
 	@Override
 	public DefaultCountingOutRhymer getHanoiRhymer() {
 		return new HanoiRhymer();
